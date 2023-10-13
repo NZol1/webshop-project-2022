ea0447107e963844b573e2856a83cbf36c4bd04f
NZol1 <100954838+NZol1@users.noreply.github.com>
2023-10-13 07:35:27 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+        <?php
+                require_once($_SERVER['DOCUMENT_ROOT']."/Webshop/scripts/includes/header.php");
+                require_once($_SERVER['DOCUMENT_ROOT']."/Webshop/scripts/includes/navigation.php");
+                require_once($_SERVER['DOCUMENT_ROOT']."/Webshop/scripts/includes/indexbody.php");
+                require_once($_SERVER['DOCUMENT_ROOT']."/Webshop/scripts/includes/footer.php");
+                DisplayMessage();
+        ?>
